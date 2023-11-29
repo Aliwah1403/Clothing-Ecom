3c418ff67d31d7a3e970e1412ea6761a6e5a807b
My pc <mypc@TamaraMacBook-Air.local>
2023-11-29 12:08:09 +0300

    changes

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const Intasend = require('intasend-node');
 
 
 // firebase admin setup
-// let serviceAccount = require("./ecom-try-fullstack-2-firebase-adminsdk-86lsr-af05397965.json");
-let serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+let serviceAccount = require("./ecom-try-fullstack-2-firebase-adminsdk-86lsr-af05397965.json");
+// let serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
